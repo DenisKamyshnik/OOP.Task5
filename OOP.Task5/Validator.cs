fdae4e9b6f0fd2768ec268b12c3a251e7cdec4c0
Denis Kamyshnik <dkamyshnik66@gmail.com>
2021-04-05 20:21:40 +0300

    Version 1.0

--- OOP.Task5/Validator.cs
+++ OOP.Task5/Validator.cs
@@ -5,6 +5,58 @@ namespace OOP.Task5
     class Validator
     {
      
+        public int InputTaskParser()
+        {
+            int i = 0;
+            int tries = 0;
 
+            while (tries++ < 100)
+            {
+                string str = Console.ReadLine();
+                if (int.TryParse(str, out i) && IsNumberIsCorrect(i))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Введите целочисленное значение от 1 до 3 включительно");
+                }
+
+            }
+            return i;
+        }
+
+        public int InputDayParser()
+        {
+            int i = 0;
+            int tries = 0;
+
+            while (tries++ < 100)
+            {
+                string str = Console.ReadLine();
+                if (int.TryParse(str, out i))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Введите целочисленное значение");
+                }
+
+            }
+            return i;
+        }
+
+        public bool IsNumberIsCorrect(int i)
+        {       
+            if (i == 1 || i == 2 || i == 3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }                    
+        }
     }
 }
